8d613b5db93591fbc3828de4b0b8a5aaeedf0d2f
Kenneth Yuen <Kenneth@Yuen.com>
2022-02-21 11:49:55 -0600

    Working version post exercise 4

---
+++ src/main/java/com/camunda/training/CreateTweetDelegate.java
@@ -0,0 +1,27 @@
+package com.camunda.training;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+import java.time.LocalDateTime;
+
+// Check me on twitter here: https://twitter.com/cmnda_demo
+public class CreateTweetDelegate implements JavaDelegate {
+    private final Logger LOGGER = LoggerFactory.getLogger(CreateTweetDelegate.class.getName());
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        String content = "I did it! Cheers tojimoto: "+ LocalDateTime.now();
+        LOGGER.info("Publishing tweet: " + content);
+        AccessToken accessToken = new AccessToken("220324559-CO8TfUmrcoCrvFHP4TacgToN5hLC8cMw4n2EwmHo", "WvVureFv5TBWTGhESgGe3fqZM7XbGMuyIhxB84zgcoUER");
+        Twitter twitter = new TwitterFactory().getInstance();
+        twitter.setOAuthConsumer("lRhS80iIXXQtm6LM03awjvrvk", "gabtxwW8lnSL9yQUNdzAfgBOgIMSRqh7MegQs79GlKVWF36qLS");
+        twitter.setOAuthAccessToken(accessToken);
+        twitter.updateStatus(content);
+    }
+}
--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,10 +1,44 @@
 package com.camunda.training;
 
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+
+@ExtendWith(ProcessEngineExtension.class)
 public class ProcessJUnitTest {
-  
-//  @Test
+
+  @Test
+  @Deployment(resources = "TwitterQA.bpmn")
   public void testHappyPath() {
-    
+    // Create a HashMap to put in variables for the process instance
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("approved", true);
+    // Start process with Java API and variables
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
+    // Make assertions on the process instance
+    assertThat(processInstance)
+            .hasPassedInOrder("Event_Started","Activity_Review","Gateway_Approval","Activity_Publish","Event_Handled")
+            .isEnded();
   }
 
+  @Test
+  @Deployment(resources = "TwitterQA.bpmn")
+  public void testRejectPath() {
+    // Create a HashMap to put in variables for the process instance
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("approved", false);
+    // Start process with Java API and variables
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
+    // Make assertions on the process instance
+    assertThat(processInstance)
+            .hasPassedInOrder("Event_Started","Activity_Review","Gateway_Approval","Activity_Reject","Event_Rejected")
+            .isEnded();
+  }
 }
